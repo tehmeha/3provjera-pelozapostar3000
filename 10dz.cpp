5cdf6c0252282b9927fdc130f3a4cbf1926c4d26
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-16 16:45:00 +0200

    Update 10dz.cpp

--- 10dz.cpp
+++ 10dz.cpp
@@ -3,13 +3,5 @@ using namespace std;
 
 int main()
 {
-    // ispis glavnog izbornika
-    cout << "Glavni izbornik" << endl;
-    cout << "1. Unos nove osobe" << endl;
-    cout << "2. Ispisi podatke" << endl;
-    cout << "3. pretraga prema racunu" << endl;
-    cout << "4. Pretraga prema prezimenu" << endl;
-    cout << "5. Ispisi sortirano" << endl;
-    int izbor;
-    cin >> izbor;
+    return 0;
 }
