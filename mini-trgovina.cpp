5a88305fd33a6e950839ba475e60cdbf8b110e2e
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-17 15:45:32 +0200

    Update mini-trgovina.cpp

--- mini-trgovina.cpp
+++ mini-trgovina.cpp
@@ -3,5 +3,34 @@ using namespace std;
 
 int main()
 {
-    return 0;
+    while(1)
+    {
+        cout << "Glavni izbornik" << endl;
+        cout << "1. " << endl;
+        cout << "2. " << endl;
+        cout << "3. " << endl;
+        cout << "4. " << endl;
+        cout << "5. " << endl;
+        int izbor;
+        int brojArtikala = 0;
+        int MAX = 50;
+        cin >> izbor;
+        if( izbor == 1 )
+        {
+        }
+        else if( izbor == 2 )
+        {
+        }
+        else if( izbor == 3 )
+        {
+        }
+        else if( izbor == 4 )
+        {
+        }
+        else if( izbor == 5 )
+        {
+            cout << "Kraj rada" << ednl;
+            break;
+        }
+    }
 }
