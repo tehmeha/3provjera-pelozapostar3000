48b9bae830ea07bf514d058bb066c91747da6a75
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-24 13:25:27 +0200

    Update mini-trgovina.cpp

--- mini-trgovina.cpp
+++ mini-trgovina.cpp
@@ -3,23 +3,35 @@ using namespace std;
 
 int main()
 {
+    int brojArtikla = 0;
+    int izbor;
+    int MAX = 50;
+    string artikli[MAX];
+
     while(1)
     {
         cout << "Glavni izbornik" << endl;
-        cout << "1. " << endl;
-        cout << "2. " << endl;
-        cout << "3. " << endl;
-        cout << "4. " << endl;
-        cout << "5. " << endl;
-        int izbor;
-        int brojArtikala = 0;
-        int MAX = 50;
+        cout << "1. Unos novog artikla" << endl;
+        cout << "2. Ispisi sve podatke" << endl;
+        cout << "3. Pretraga prema bar kodu artikla" << endl;
+        cout << "4. Pretraga prema nazivu artikla" << endl;
+        cout << "5. Ispisi sortirano prema nazivu artikla" << endl;
+        cout << "6. Izlaz iz programa" << endl;
+        cout << "Upisite broj iz izbornika: ";
         cin >> izbor;
         if( izbor == 1 )
         {
+            cout << "Unesite naziv artikla: ";
+            cin.ignore();
+            getline(cin, artikli[brojArtikla]);
+            brojArtikla++;
         }
         else if( izbor == 2 )
         {
+            for( int i = 0; i < brojArtikla; i++ )
+            {
+                cout << artikli[i] << endl;
+            }
         }
         else if( izbor == 3 )
         {
@@ -29,8 +41,16 @@ int main()
         }
         else if( izbor == 5 )
         {
-            cout << "Kraj rada" << ednl;
+        }
+        else if( izbor == 6 )
+        {
+            cout << "Kraj rada" << endl;
             break;
         }
+        else
+        {
+            cout << "Unijeli ste krivi broj." << endl;
+        }
+        cout << endl;
     }
 }
