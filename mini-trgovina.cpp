04313cf17a5b451e8509941820e2d2437a0935c4
PapaFranku3000 <leonardo.peloza5@gmail.com>
2017-05-31 11:09:50 +0200

    unos nove osobe

--- mini-trgovina.cpp
+++ mini-trgovina.cpp
@@ -8,6 +8,8 @@ int main()
     int izbor;
     int MAX = 50;
     string artikli[MAX];
+    unsigned long long int barkod[MAX];
+    float cijena [MAX];
     ofstream datotekaUpisivanje;
     ifstream datotekaUcitavanje;
 
@@ -25,9 +27,13 @@ int main()
         cin >> izbor;
         if( izbor == 1 )
         {
+            cout << "unesite bar kod artikla:";
+            cin >>barkod[brojArtikla];
             cout << "Unesite naziv artikla: ";
             cin.ignore();
             getline(cin, artikli[brojArtikla]);
+            cout << "unesite cijena artikla:";
+            cin >> cijena[brojArtikla];
             brojArtikla++;
         }
         else if( izbor == 2 )
